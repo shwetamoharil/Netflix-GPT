63740a9f30950305dafbb403b5784e790cc25ae0
ShwetaMoharil-GSLab <shweta.moharil@gslab.com>
2023-11-08 17:16:05 +0530

    feat: added Header comp

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,17 @@
+import Header from "./Header";
+
 const Login = () => {
-  return <div>This is Login</div>;
+  return (
+    <div>
+      <Header />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/ab4b0b22-2ddf-4d48-ae88-c201ae0267e2/0efe6360-4f6d-4b10-beb6-81e0762cfe81/IN-en-20231030-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="background-logo"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
