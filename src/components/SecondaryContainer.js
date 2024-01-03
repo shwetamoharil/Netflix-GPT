d35f4c23afa12b8e32d789beb5cb86325c6d45dd
shwetamoharil <shwetavmmoharil@gmail.com>
2024-01-03 17:04:59 +0530

    feat: added popular movies list api

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -2,15 +2,16 @@ import { useSelector } from "react-redux";
 import MovieList from "./MovieList";
 
 const SecondaryContainer = () => {
-  const nowPlayingMovies = useSelector(
-    (store) => store?.movies?.nowPlayingMovies
+  const {nowPlayingMovies, popularMovies} = useSelector(
+    (store) => store?.movies
   );
+  
   return (
     nowPlayingMovies && (
       <div className="bg-black ">
         <div className="-mt-52 relative z-20">
           <MovieList title={"Now Playing"} movies={nowPlayingMovies} />
-          <MovieList title={"Trending"} movies={nowPlayingMovies} />
+          <MovieList title={"Popular Movies"} movies={popularMovies} />
           <MovieList title={"Thriller"} movies={nowPlayingMovies} />
         </div>
       </div>
--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -8,7 +8,7 @@ const useNowPlayingMovies = () => {
   const { addNowPlayingMovies } = moviesAction;
 
   const getNowPlayingMovie = async () => {
-    const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1", API_OPTIONS);
+    const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1", API_OPTIONS);
     const json = await data.json();
     dispatch(addNowPlayingMovies(json?.results));
   };
